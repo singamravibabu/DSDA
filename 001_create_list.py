26d3e2bfce6f30631deabe363948d2706671c4fd
Singam Ravi Babu <Singam Ravi Babu>
2025-07-09 19:58:00 +0530

    second commit

---
+++ 001_create_list.py
@@ -0,0 +1,16 @@
+# list with five floats
+temps = [32.8, 34.3, 29.1, 30.7, 19.3]
+print(temps)
+
+
+# list with six strings
+stationary = ["book", "pencil", "pen", "papers", "erasor", "sharpener"]
+print(stationary)
+
+# list with 3 items - str, int, and float values
+book = ["Attitude", 2002, 199.0]
+print(book)
+
+# empty list
+marks = []
+print(marks)
--- listObject.md
+++ listObject.md
@@ -1,3 +1,8 @@
 # WORKING WITH LISTS
 > A list is a collection of items.
 
+##### The syntax for creating a list
+list_name = [item1, item2, item3, ...]
+
+##### We can use the repetition operator (*) to repeat the items int he list
+*[item] * int*
